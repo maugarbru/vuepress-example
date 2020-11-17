663674285fff2c4f773fb59c8ce9cb2b62f87155
Mauricio G <mauriciogarzonb@gmail.com>
2020-11-17 11:28:11 -0500

    Loss init

---
+++ components/Loss.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-stepper v-model="e6" vertical>
+    <v-stepper-step :complete="e6 > 1" step="1">
+      Subir archivo de datos
+      <small>Archivo .xls o .csv</small>
+    </v-stepper-step>
+
+    <v-stepper-content step="1">
+      <v-container>
+        <v-file-input
+          v-model="file"
+          chips
+          show-size
+          label="Archivo de datos"
+          placeholder="Sube tu archivo"
+          prepend-icon="mdi-file-excel"
+        ></v-file-input>
+        <v-btn :disabled="!file" outlined color="primary" @click="e6 = 2">
+          Calcular
+        </v-btn>
+      </v-container>
+    </v-stepper-content>
+
+    <v-stepper-step :complete="e6 > 2" step="2">
+      Calcular pérdida
+    </v-stepper-step>
+
+    <v-stepper-content step="2">
+      <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+    </v-stepper-content>
+
+    <v-stepper-step :complete="e6 > 3" step="3">
+      Ver resultados
+    </v-stepper-step>
+
+    <v-stepper-content step="3">
+      <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+      <v-btn color="primary" @click="e6 = 4"> Continue </v-btn>
+      <v-btn text> Cancel </v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step step="4"> Generar reporte </v-stepper-step>
+    <v-stepper-content step="4">
+      <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+      <v-btn color="primary" @click="e6 = 1"> Continue </v-btn>
+      <v-btn text> Cancel </v-btn>
+    </v-stepper-content>
+  </v-stepper>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      e6: 1,
+      file: null,
+    };
+  },
+};
+</script>
--- pages/app.vue
+++ pages/app.vue
@@ -3,12 +3,13 @@
     <v-dialog v-model="dialog" persistent max-width="500">
       <auth v-if="dialog" @login="login" />
     </v-dialog>
-    <v-card outlined v-if="user" class="mx-auto" max-width="700">
+    <v-card outlined v-if="user" class="mx-auto">
       <v-app-bar color="transparent" dense flat>
         <h2 class="primary--text">Bienvenido</h2>
         <v-spacer></v-spacer>
         <v-btn @click="cerrarSesion()" small color="red" outlined
-          >Cerrar sesión <v-icon right>mdi-account-remove-outline</v-icon></v-btn
+          >Cerrar sesión
+          <v-icon right>mdi-account-remove-outline</v-icon></v-btn
         >
       </v-app-bar>
       <v-card-text>
@@ -16,21 +17,21 @@
           <template v-slot:default>
             <tbody>
               <tr>
-                <td>
+                <td align="right">
                   <v-icon color="success" left>mdi-domain</v-icon
                   ><b class="success--text mr-4">Compañía</b>
                 </td>
                 <td>{{ user.companyName }}</td>
               </tr>
               <tr>
-                <td>
+                <td align="right">
                   <v-icon color="success" left>mdi-account</v-icon
                   ><b class="success--text mr-4">Usuario</b>
                 </td>
                 <td>{{ user.userName }}</td>
               </tr>
               <tr>
-                <td>
+                <td align="right">
                   <v-icon color="success" left>mdi-email</v-icon
                   ><b class="success--text mr-4">E-mail</b>
                 </td>
@@ -41,19 +42,49 @@
         </v-simple-table>
       </v-card-text>
     </v-card>
+    <br />
+    <v-card>
+      <v-tabs
+        v-model="tab"
+        background-color="grey lighten-3"
+        color="primary"
+        grow
+      >
+        <v-tab> Pérdida </v-tab>
+        <v-tab> Recuperación </v-tab>
+      </v-tabs>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-card flat>
+            <loss />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <recovery />
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
   </v-container>
 </template>
 
 <script>
 import Auth from "@/components/Auth.vue";
+import Loss from "@/components/Loss.vue";
+import Recovery from "@/components/Recovery.vue";
 
 export default {
   components: {
     Auth,
+    Loss,
+    Recovery
   },
   layout: "app",
   data() {
     return {
+      tab: 0,
       dialog: true,
       user: undefined,
     };
@@ -67,9 +98,9 @@ export default {
     },
 
     cerrarSesion() {
-      this.user = undefined
-      this.dialog = true
-    }
+      this.user = undefined;
+      this.dialog = true;
+    },
   },
 };
 </script>
